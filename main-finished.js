f3e4948099caa9f25feeb30fa9cd4cd242cc38ff
sinan-sulaiman-33 <143943603+sinan-sulaiman-33@users.noreply.github.com>
2023-12-29 17:32:32 +0530

    Defining EvilCircle

--- main-finished.js
+++ main-finished.js
@@ -86,9 +86,33 @@ collisionDetect() {
       }
     }
   }
+ }
 }
 
+class EvilCircle extends Space {
+  constructor(x,y) {
+    super(x, y, 20, 20);
+    this.color = "white";
+    this.size = 10;
+    
+    window.addEventListener("keydown", (e) => {
+  switch (e.key) {
+    case "a":
+      this.x -= this.velX;
+      break;
+    case "d":
+      this.x += this.velX;
+      break;
+    case "w":
+      this.y -= this.velY;
+      break;
+    case "s":
+      this.y += this.velY;
+      break;
+  }
+});
 
+  }
 }
 
 const balls = [];
