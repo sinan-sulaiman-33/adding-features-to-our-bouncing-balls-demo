601ea13397384dca74935c0bece8f40b952bafbb
sinan-sulaiman-33 <143943603+sinan-sulaiman-33@users.noreply.github.com>
2023-12-29 21:57:12 +0530

    Update and rename main-finished.js to main.js

--- main.js
+++ main.js
@@ -39,16 +39,6 @@ class Ball extends Shape {
   }
 }
 
-class Ball {
-  constructor(x, y, velX, velY, color, size) {
-    this.x = x;
-    this.y = y;
-    this.velX = velX;
-    this.velY = velY;
-    this.color = color;
-    this.size = size;
-  }
-
    draw() {
       ctx.beginPath();
       ctx.fillStyle = this.color;
