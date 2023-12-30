f3b3538fc0e363b90bc699ed9f55493afd7e28ce
sinan-sulaiman-33 <143943603+sinan-sulaiman-33@users.noreply.github.com>
2023-12-30 16:12:31 +0530

    Update finalcode.js

--- finalcode.js
+++ finalcode.js
@@ -1,6 +1,5 @@
 const para = document.querySelector('p');
 let count = 0;
-//wrong above
 
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
@@ -10,19 +9,12 @@ const height = canvas.height = window.innerHeight;
 
 function random(min,max) {
   const num = Math.floor(Math.random()*(max-min)) + min;
-  //above line return Math.floor(Math.random() * (max - min + 1)) + min;
-  //nothing below
   return num;
 };
 
 function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
-
-//class Ball
-
-//no class Shape
-//no class Ball extends Shape
 
 class Shape {
   constructor(x, y, velX, velY) {
@@ -51,19 +43,19 @@ class Ball extends Shape {
 
    update() {
       if ((this.x + this.size) >= width) {
-         this.velX = -(this.velX); //different value after equal sign
+         this.velX = -(this.velX);
       }
 
       if ((this.x - this.size) <= 0) {
-         this.velX = -(this.velX); //different value after equal sign
+         this.velX = -(this.velX);
       }
 
       if ((this.y + this.size) >= height) {
-         this.velY = -(this.velY); //different value after equal sign
+         this.velY = -(this.velY);
       }
 
       if ((this.y - this.size) <= 0) {
-         this.velY = -(this.velY); //different value after equal sign
+         this.velY = -(this.velY);
       }
 
       this.x += this.velX;
@@ -84,15 +76,12 @@ collisionDetect() {
   }
  }
 
-//no class EvilCircle extends Space
-
 class EvilCircle extends Space {
   constructor(x,y) {
     super(x, y, 20, 20);
     this.color = "white";
     this.size = 10;
     
-    //no window.addEventListener
     
     window.addEventListener("keydown", (e) => {
       switch (e.key) {
@@ -111,7 +100,6 @@ class EvilCircle extends Space {
       }
     });
 }
-  //no draw ()
   draw() {
       ctx.beginPath();
       ctx.lineWidth = 3;
@@ -120,7 +108,6 @@ class EvilCircle extends Space {
       ctx.stroke();
   }
 
-  //no checkBounds()
   checkBounds() {
       if ((this.x + this.size) >= width) {
          this.x -= this.size;
@@ -135,7 +122,6 @@ class EvilCircle extends Space {
          this.y += this.size;
       }
    }
-   //no collisonDetect
   collisionDetect() {
     for (const ball of balls) {
       if (ball.exists) {
@@ -166,12 +152,10 @@ while (balls.length < 25) {
    );
 
   balls.push(ball);
-  //no text below
   count++;
   para.textContent = 'Ball count: ' + count;
 }
 
-//no const Eball
 const EBall = new EvilCircle(random(0,width),random(0,height));
 
 function loop() {
@@ -179,14 +163,12 @@ function loop() {
    ctx.fillRect(0, 0,  width, height);
 
    for (const ball of balls) {
-    //no if statement
      if (ball.exists) {
      ball.draw();
      ball.update();
      ball.collisionDetect();
      }
    }
-   //no text below
    EBall.draw();
    EBall.checkBounds();
    EBall.collisionDetect();
